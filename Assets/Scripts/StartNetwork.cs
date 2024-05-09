1f0c00cf7e7e8af392bebb0afb83b81a84035da9
Radu <aliment4476@gmail.com>
2024-05-09 16:56:34 +0300

    Fixed clients not connecting to host
    Added method for the host to see their IP and method for clients to connect to host's IP

--- Assets/Scripts/StartNetwork.cs
+++ Assets/Scripts/StartNetwork.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
 using System.Runtime.CompilerServices;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode.Transports.UTP;
 
 public class StartNetwork : MonoBehaviour
 {
     [SerializeField] private GameObject fadeImage;
     [SerializeField] private GameObject waitingText;
+    [SerializeField] private TextMeshProUGUI ipAddressText;
+    [SerializeField] private TMP_InputField ipInput;
+
+    private string ipAddress;
+    private UnityTransport transport;
+
+    private void Start()
+    {
+        ipAddress = "0.0.0.0";
+        SetIpAddress();
+    }
 
     private void Update()
     {
@@ -18,21 +34,49 @@ public class StartNetwork : MonoBehaviour
         }
     }
 
-    public void StartClient()
-    {
-        NetworkManager.Singleton.StartClient();
-    }
-
     public void StartHost()
     {
         NetworkManager.Singleton.StartHost();
+        GetLocalIPAddress();
         waitingText.SetActive(true);
+    }
+
+    public void StartClient()
+    {
+        ipAddress = ipInput.text;
+        SetIpAddress();
+        NetworkManager.Singleton.StartClient();
     }
 
     private void BeginGame()
     {
         fadeImage.GetComponent<Animator>().enabled = true;
         waitingText.SetActive(false);
+        ipAddressText.gameObject.SetActive(false);
+        ipInput.gameObject.SetActive(false);
         gameObject.SetActive(false);
+    }
+
+    private string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                ipAddressText.text = ip.ToString();
+                ipAddress = ip.ToString();
+                return ip.ToString();
+            }
+        }
+
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    private void SetIpAddress()
+    {
+        transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.ConnectionData.Address = ipAddress;
     }
 }
